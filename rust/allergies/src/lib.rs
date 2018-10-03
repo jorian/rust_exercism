a1174659283c419d80cfb6247579e52e2927c6a7
Jorian <jorian@outlook.com>
2018-10-03 18:42:26 +0200

    allergies

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,7 +1,23 @@
+#[macro_use]
+extern crate strum_macros;
+extern crate strum;
 
+use strum::IntoEnumIterator;
+
+#[derive(Clone, Copy, PartialEq, Debug, EnumIter)]
+pub enum Allergen {
+    Eggs           = 1 << 0,
+    Peanuts        = 1 << 1,
+    Shellfish      = 1 << 2,
+    Strawberries   = 1 << 3,
+    Tomatoes       = 1 << 4,
+    Chocolate      = 1 << 5,
+    Pollen         = 1 << 6,
+    Cats           = 1 << 7,
+}
 
 pub struct Allergies {
-    score: u8
+    score: u8,
 }
 
 impl Allergies {
@@ -10,38 +26,19 @@ impl Allergies {
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        let result = vec![8; Allergen];
+        let mut output = Vec::new();
+
+        for i in 0..8 {
+            match self.score & (1<<i) != 0 {
+                true  => output.push(Allergen::iter().nth(i).unwrap()),
+                _     => ()
+            }
+        }
+
+        output
     }
 
     pub fn is_allergic_to(&self, alg: &Allergen) -> bool {
-
+        self.score & (*alg as u8) != 0
     }
 }
-
-pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
-}
-
-fn check_score(score: u8) -> u8 {
-    match score {
-        score if score << 0 => 1,
-
-    }
-}
-
-pub fn is_allergic_to(a: &Allergen) -> bool {
-
-}
-
-pub fn contains(a: &Allergen) -> bool {
-
-}
-
-// https://www.includehelp.com/c/how-to-check-a-particular-bit-is-set-or-not-using-c-program.aspx
